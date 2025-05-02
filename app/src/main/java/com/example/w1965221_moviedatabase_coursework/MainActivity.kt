f6b581513a208490f6fe4805553b4e5cf180ee6a
Alek1010 <ali.mihaylov04@gmail.com>
2025-05-02 13:27:47 +0100

    inital commit

---
+++ app/src/main/java/com/example/w1965221_moviedatabase_coursework/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.w1965221_moviedatabase_coursework
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.w1965221_moviedatabase_coursework.ui.theme.W1965221_MovieDataBase_CourseworkTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            W1965221_MovieDataBase_CourseworkTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    W1965221_MovieDataBase_CourseworkTheme {
+        Greeting("Android")
+    }
+}
